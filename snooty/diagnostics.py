8ceb7fea73969116206f32b144e513755a551ea8
madelinezec <maddie.zechar@mongodb.com>
2020-04-22 15:56:22 -0400

    lint

--- snooty/diagnostics.py
+++ snooty/diagnostics.py
@@ -193,6 +193,7 @@ class MissingRef(Diagnostic):
         super().__init__(f"Missing ref; all {name} must define a ref", start, end)
         self.name = name
 
+
 class MissingDefinitionList(Diagnostic):
     severity = Diagnostic.Level.error
 
@@ -201,7 +202,11 @@ class MissingDefinitionList(Diagnostic):
         start: Union[int, Tuple[int, int]],
         end: Union[None, int, Tuple[int, int]] = None,
     ) -> None:
-        super().__init__(f"Missing definition list; all glossaries must define a definition list", start, end)
+        super().__init__(
+            f"Missing definition list; all glossaries must define a definition list",
+            start,
+            end,
+        )
 
 
 class FailedToInheritRef(Diagnostic):
